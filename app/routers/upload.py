9c17a734784f19673a82a3b972207397a5bc5a41
Dev PC <DewaltSnyman@outlook.com>
2026-02-18 01:17:17 +0200

    frontend update

--- app/routers/upload.py
+++ app/routers/upload.py
@@ -4,6 +4,8 @@ from app.db import get_db
 from app.services.import_service import import_items, import_panel_tests
 import shutil
 import os
+from fastapi.responses import RedirectResponse
+
 
 router = APIRouter(prefix="/upload", tags=["Upload"])
 
@@ -15,11 +17,19 @@ def upload_solar(file: UploadFile = File(...), db: Session = Depends(get_db)):
     with open(temp_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    import_items(db, temp_path)
+    try:
+        import_items(db, temp_path)
+        message = "solar_success"
+    except Exception:
+        message = "solar_error"
 
     os.remove(temp_path)
 
-    return {"message": "Solar units imported"}
+    return RedirectResponse(
+        url=f"/?status={message}",
+        status_code=303
+    )
+
 
 
 @router.post("/tests")
@@ -30,8 +40,16 @@ def upload_tests(file: UploadFile = File(...), db: Session = Depends(get_db)):
     with open(temp_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    import_panel_tests(db, temp_path)
+    try:
+        import_panel_tests(db, temp_path)
+        message = "tests_success"
+    except Exception:
+        message = "tests_error"
 
     os.remove(temp_path)
 
-    return {"message": "Panel tests imported"}
+    return RedirectResponse(
+        url=f"/?status={message}",
+        status_code=303
+    )
+
